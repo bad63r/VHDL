770ab5fff1042bcee975e8a514b75b9a4af8a8ce
bad63r <bad63r@gmail.com>
2017-04-18 19:47:04 +0200

    18.4.2017

--- binary_division_algorithm/binary_division_algorithm.vhd
+++ binary_division_algorithm/binary_division_algorithm.vhd
@@ -63,6 +63,55 @@ begin  -- architecture rtl
         next_state <= idle;
     end case;
   end process;
-  
+
+  --controlpath output logic
+  ready <= '1' when (current_state = idle) else '0';
+
+  --datapath registers
+  process(clk,reset)
+  begin
+    if reset = '1' then
+      op1_reg <= (others => '0');
+      op2_reg <= (others => '0');
+      q_reg   <= (others => '0');
+      r_reg   <= (others => '0');
+    elsif rising_edge(clk) then
+      op1_reg <= op1_next;
+      op2_reg <= op2_next;
+      q_reg   <= q_next;
+      r_reg   <= r_next;
+    end if;
+  end process;
+
+  --datapath routing network
+  process(current_state,next_state,op1_reg,op1_next,op2_reg,op2_next,q_reg,q_next,r_reg,r_next,a_in,b_in)
+  begin
+    case current_state is
+      when idle =>
+        op1_next <= unsigned(a_in);
+        op2_next <= unsigned(b_in);
+        q_next   <= (others => '0');
+        r_next   <= (others => '0');
+      when reverse =>
+        op1_next <= unsigned(b_in);
+        op2_next <= unsigned(a_in);
+        q_next   <= (others => '0');
+        r_next   <= (others => '0');
+      when division =>
+        op1_next <= op1_reg - op2_reg;
+        op2_next <= op2_reg;
+        q_next   <= q_reg + 1;
+        r_next   <= (others => '0');
+      when finit =>
+        op1_next <= op1_reg;
+        op2_next <= op2_reg;
+        q_next   <= q_reg;
+        r_next   <= op1_reg;
+    end case;
+  end process;
+
+  --output
+  result <= std_logic_vector(q_reg);
+  leftovers <= std_logic_vector(r_reg);
 
 end architecture rtl;
---
+++ binary_division_algorithm/binary_division_algorithm_tb.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+entity binary_division_tb is
+  
+end entity binary_division_tb;
+
+architecture rtl of binary_division_tb is
+
+  constant WIDTH : natural := 8;
+  signal a_in, b_in        : std_logic_vector(WIDTH-1 downto 0);
+  signal clk, reset        : std_logic;
+  signal start             : std_logic;
+  signal result, leftovers : std_logic_vector(WIDTH-1 downto 0);
+  signal ready             : std_logic;
+
+  
+begin  -- architecture rtl
+
+  binary_division_1: entity work.binary_division
+    generic map (
+      WIDTH => WIDTH)
+    port map (
+      a_in      => a_in,
+      b_in      => b_in,
+      clk       => clk,
+      reset     => reset,
+      start     => start,
+      result    => result,
+      leftovers => leftovers,
+      ready     => ready);
+
+    stim_gen_clk:process
+    begin
+      clk <= '0','1' after 25 ns;
+      wait for 50 ns;
+    end process;
+
+  stim_gen:process
+  begin
+    reset <= '1','0' after 5 ns,'1' after 1500 ns;
+    start <= '1';
+    a_in  <= "00010000", "00000100" after 1000 ns;
+    b_in  <= "00000010", "00001100" after 1000 ns;
+    wait;
+  end process;
+
+end architecture rtl;
