f3ce1d88ff992b0666b227ea09f254a92dfd0ec0
bad63r <bad63r@gmail.com>
2017-04-18 17:03:41 +0200

    18.4.2017

--- binary_division_algorithm/binary_division_algorithm.vhd
+++ binary_division_algorithm/binary_division_algorithm.vhd
@@ -11,13 +11,20 @@ entity binary_division is
     a_in, b_in       : in  std_logic_vector(WIDTH-1 downto 0);
     clk, reset       : in  std_logic;
     start            : in  std_logic;
-    result, lefovers : out std_logic_vector(WIDTH-1 downto 0);
+    result,leftovers : out std_logic_vector(WIDTH-1 downto 0);
     ready            : out std_logic);
 
 end entity binary_division;
 
 architecture rtl of binary_division is
 
+  type state is (idle,reverse,division,finit);
+  signal current_state,next_state : state;
+  signal op1_reg,op1_next : unsigned(WIDTH-1 downto 0);
+  signal op2_reg,op2_next : unsigned(WIDTH-1 downto 0);
+  signal q_reg,q_next : unsigned(WIDTH-1 downto 0);
+  signal r_reg,r_next : unsigned(WIDTH-1 downto 0);
+  
 begin  -- architecture rtl
 
   --controlpath register
@@ -31,6 +38,31 @@ begin  -- architecture rtl
   end process;
 
   --controlpath generating next_state logic
-  process(start,current_state,
+  process(start,current_state,op1_reg,op1_next,op2_next,op2_reg,q_reg,q_next,r_reg,r_next,a_in,b_in)
+  begin
+    case current_state is
+      when idle =>
+        if start = '1' then
+          if (a_in < b_in) then
+            next_state <= reverse;
+          else
+            next_state <= division;
+          end if;
+        else
+          next_state <= idle;
+        end if;
+      when reverse =>
+        next_state <= division;
+      when division =>
+        if (op1_next < op2_next) then
+          next_state <= finit;
+        else
+          next_state <= division;
+        end if;
+      when finit =>
+        next_state <= idle;
+    end case;
+  end process;
+  
 
 end architecture rtl;
