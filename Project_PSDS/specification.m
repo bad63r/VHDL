b63668fe29366d8b84ac623b1e22b5e81a8b30c1
bad63r <bad63r@gmail.com>
2019-02-18 15:27:27 +0100

    improving spec for the project

--- Project_PSDS/specification.m
+++ Project_PSDS/specification.m
@@ -1,19 +1,32 @@
-%specifikacija projekta iz PSDS
+% Specification of PSDS project
+%-------------------------------
 
-%definisanje frekfencije odabiranja
-
-% This is unnecessary, wavread will return that
-% fs = 22050;
-%ucitavanje signala test.wav u promenjivu uzorak
-[uzorak,fs] = wavread('test.wav');
-ch1 = uzorak(:, 1);
-ch2 = uzorak(:, 2);
-%grafik signala
-%vreme
-% t=0:0.00001:300;
+% Audio_sig is variable into which signal will be loaded and fs is sample frequency of audio signal
+[audio_sig,fs] = audioread('/home/bad63r/Projects/matlab/proba.wav');
+ch1 = audio_sig(:, 1);
+ch2 = audio_sig(:, 2);
+% Time
 % This is delta time between two samples.
 dt = 1/fs;
 % This is how we can define time now.
-t = 0:dt:(length(ch1)-1)*dt;
-plot(t,ch1,'k')
+t1 = 0:dt:(length(ch1)-1)*dt;
+t2 = 0:dt:(length(ch2)-1)*dt;
+
+% First graph - ch1
+subplot(2,1,1)
+plot(t1,ch1,'g');
+title('channel 1');
+xlabel('time');
+ylabel('amplitude');
+
+% Second graph - ch2
+subplot(2,1,2)
+plot(t2,ch2,'r');
+title('channel 2');
+xlabel('time');
+ylabel('amplitude');
 hold on;
+
+% FFT
+
+
